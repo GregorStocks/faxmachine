f45d21c656520dfb7b178a989f7e0eb06ce880e0
Kurper <kurper@gmail.com>
2011-04-09 22:41:10 -0400

    it's no longer really php, woo

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
+<!doctype html>
 <html>
 <head>
 <title>Useless Facts</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript">
+function get_fact() {
+    var facts = ["Robots will one day destroy us all.",
+                 "Iris recognition is pretty cool.",
+                 "e^i&pi; = -1",
+                 "poop",
+                 "math"
+                 ];
+    var fact = facts[Math.floor(Math.random() * facts.length)];
+    return fact;
+}
+function nextfact() {
+    $("#fact").html(get_fact());
+} 
+$(document).ready(function () {
+    nextfact();
+});
+</script>
 </head>
 <body>
-<?php
-function get_fact() {
-    $facts = array("Robots will one day destroy us all.",
-                   "Iris recognition is pretty cool.",
-                   "e^i&pi; = -1",
-                   "poop",
-                   "math"
-                  );
-    return $facts[rand(0, count($facts) - 1)];
-}
-
-for($i = 0; $i < 5; $i++) {
-    ?><p><?=get_fact()?></p><?php
-}
-?>
+<div class="outer">
+    <div class="fact"><p id="fact">
+    </p></div>
+    <button type="button" onclick="nextfact()">Next</button>
+</div>
 </body>
 </html>
