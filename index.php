e062a29521fbde352277215c0aba5a2cbb27b352
Kurper <kurper@gmail.com>
2011-04-10 16:30:24 -0400

    minor layout changes

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 <div class="outer">
     <div class="fact">
         <p id="fact"></p>
+        <p style="display:none" id="morefact"></p>
         <span id="tags">&nbsp;</span><span id="tell"><a href="javascript:void(0)" onclick="javascript:tellmemore(true)">tell me more...</a></span>
         <div style="clear:both;"></div>
-        <p style="display:none" id="morefact"></p>
     </div>
     <button id="next" type="button" onclick="get_fact()">Next fact</button>
 </div>
