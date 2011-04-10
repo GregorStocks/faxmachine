aa594d95aa3f88d24870c3d5f708cd732de562b7
Kurper <kurper@gmail.com>
2011-04-09 22:04:57 -0400

    now with 5x the facts too!!!

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
 <body>
 <?php
 function get_fact() {
-    return "Robots will one day destroy us all.";
+    $facts = array("Robots will one day destroy us all.",
+                   "Iris recognition is pretty cool.",
+                   "e^i&pi; = -1",
+                   "poop",
+                   "math"
+                  );
+    return $facts[rand(0, count($facts) - 1)];
 }
 
 for($i = 0; $i < 5; $i++) {
