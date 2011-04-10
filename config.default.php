f4e2d30b6e77ef1b7ef5d9a2ba75d865b2b190c1
Kurper <kurper@gmail.com>
2011-04-10 16:32:47 -0400

    no more fb probably

--- config.default.php
+++ config.default.php
@@ -2,10 +2,4 @@
 function get_connection() {
     return new mysqli("localhost", "facts", "facts", "facts"); // change that
 }
-function make_facebook() {
-    return new Facebook(
-        'appId' => '117743971608120', // change these
-        'secret' => '943716006e74d9b9283d4d5d8ab93204',
-        'cookie' => true,
-    ));
 ?>
